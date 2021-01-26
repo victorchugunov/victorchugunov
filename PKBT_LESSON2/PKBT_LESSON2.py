1e5fb0aa6f16dde579fae696a7c158d6a897379d
Victor Chugunov <victor.chugunov@pkbtrend.com>
2021-01-26 12:45:42 +0300

    LESSON2_HW

---
+++ PKBT_LESSON2/PKBT_LESSON2.py
@@ -0,0 +1,46 @@
+#task1 написать функцию 1/math.log(N)
+#task2 написать функцию, возвращающую список множителей аргумента
+#task3 построить график (посчитать) разницы между реальным
+#значением процента простых чисел и асимптоты
+
+import math
+
+def asymptote(X): #функция вычисления количества простых чисел от 1 до аргумента
+    result = (X/math.log(X))
+    return result
+    
+def factoring(Y): #функция определения списка множителей аргумента
+    factor = []
+    devider = 2
+    while Y >= math.sqrt(devider):
+        if Y % devider == 0:
+            factor.append(devider)
+            Y = Y/devider
+        else:
+           devider += 1
+    if Y>1:
+        factor.append(n)
+    return factor
+
+end = int(input ("end = ")) #ввод верхней границы "графика"
+
+#график строим с шагом 10 от 10 до end
+for N in range (10, int(end), 10):
+    count = 0
+    for x in range(1, int(N)):
+        is_x_prime = x != 1
+        
+        for n in range(2, round(math.sqrt(x)+1)):
+            if (x % n) == 0:
+                is_x_prime = False
+                break
+
+        if is_x_prime:
+            count += 1
+    print ("N = ", N)
+    print("difference = ",round(count-asymptote(N),2)) #разница значений количества простых чисел
+
+    #print("#"*int(round(count-asymptote(N)))) #принт графика
+
+    #список множителей числа "end":
+print("factor 'end' = ", factoring(end))
